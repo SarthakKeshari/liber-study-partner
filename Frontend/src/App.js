3726db22c3589aad178bf83496eb8cf60831cbcc
Anushka Srivastava <anushka-srivastava22>
2022-07-29 13:49:43 +0530

    Made Changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,53 +4,15 @@ import Navbar from './components/Navbar';
 import './App.css'
 import LeftPanel from './components/Dashboard/LeftPanel';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import CourseDetails from './components/Courses/CourseDetails'
-<<<<<<< HEAD
-//import Trivia from './components/TechTrivia/Trivia'
-=======
-import TriviaQuizPanel from './components/TechTrivia/TriviaQuizPanel'
-import Trivia from './components/TechTrivia/Trivia';
-<<<<<<< HEAD
->>>>>>> 27cabb86092c3db36de7de30b1c09a7decabb6d0
-=======
->>>>>>> c24fa442c4664871186862f9fe7f187e5955d738
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
 
-const theme = extendTheme({ colors })
-
-// 3. Pass the `theme` prop to the `ChakraProvider`
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Navbar/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <LeftPanel /> 
-=======
-=======
->>>>>>> c24fa442c4664871186862f9fe7f187e5955d738
-      <BrowserRouter>
-          <Routes>
-              <Route exact path="/" element={<HomePage />} />
-              <Route exact path="/trivia" element={<Trivia  />} />
-            {/* <TriviaQuizPanel/> */}
-            {/* <CourseDetails/> */}
-          </Routes>
-        </BrowserRouter>
-<<<<<<< HEAD
->>>>>>> 27cabb86092c3db36de7de30b1c09a7decabb6d0
-=======
->>>>>>> c24fa442c4664871186862f9fe7f187e5955d738
+      <LeftPanel />
       <Footer/>
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
