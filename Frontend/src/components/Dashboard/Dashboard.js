b5fb5b6d43a385e8127f80785acdf1809dbfa9e1
Anushka Srivastava <anushka-srivastava22>
2022-07-30 17:01:00 +0530

    Added Dashboard

---
+++ Frontend/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import LeftPanel from './LeftPanel'
+import MainPanel from './MainPanel'
+
+export default function Dashboard() {
+    return (
+        <Box>
+            <LeftPanel />
+            <MainPanel />
+        </Box>
+    );
+}
